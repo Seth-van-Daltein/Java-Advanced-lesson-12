3f33dc962df56b72171fb46bedc3a1c7cc5b1610
Seth-van-Daltein <sethvandalstein@gmail.com>
2023-06-30 16:27:42 +0300

    added lesson-12-gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Мене звуть Максим Куземський, і я радий вчитись із LOGOS IT ACADEMY");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>My name Максим Куземський, і я радий вчитись із LOGOS IT ACADEMY</h2>
+</body>
+</html>
